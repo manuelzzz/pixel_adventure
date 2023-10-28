67b02da44e66610fe5ae09b7cf682325f0d4dcf7
Manel <manuelss.23.bil@gmail.com>
2023-10-27 22:09:49 -0300

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:pixel_adventure/pixel_adventure.dart';
+
+void main() {
+  runApp(GameWidget(
+    game: PixelAdventure(),
+  ));
+}
