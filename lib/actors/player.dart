a9a6c1f4cedb1619ce6de2c976dd277d3408e01c
Manel <manuelss.23.bil@gmail.com>
2023-10-28 14:46:30 -0300

    feat: player movement to right, left and stop. added to a little bit of documentation into functions

--- lib/actors/player.dart
+++ lib/actors/player.dart
@@ -4,12 +4,19 @@ import 'package:pixel_adventure/pixel_adventure.dart';
 
 enum PlayerState { idle, running }
 
+enum PlayerDirection { left, right, none }
+
 class Player extends SpriteAnimationGroupComponent
     with HasGameRef<PixelAdventure> {
   late final SpriteAnimation idleAnimation;
   late final SpriteAnimation runningAnimation;
   final double stepTime = 0.05;
   final String character;
+
+  PlayerDirection playerDirection = PlayerDirection.left;
+  double moveSpeed = 100;
+  Vector2 velocity = Vector2.zero();
+  bool facingRight = true;
 
   Player({
     required this.character,
@@ -20,6 +27,12 @@ class Player extends SpriteAnimationGroupComponent
   FutureOr<void> onLoad() {
     _loadAllAnimations();
     return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    _updatePlayerMovement(dt);
+    super.update(dt);
   }
 
   void _loadAllAnimations() {
@@ -36,6 +49,9 @@ class Player extends SpriteAnimationGroupComponent
     current = PlayerState.running;
   }
 
+  /// `amount` the number of frames that the .png file have
+  ///
+  /// `state` like idle, running and hit (see all at /assets/images/Main Characters/)
   SpriteAnimation _spriteAnimation({
     required int amount,
     required String state,
@@ -48,5 +64,40 @@ class Player extends SpriteAnimationGroupComponent
         textureSize: Vector2.all(32),
       ),
     );
+  }
+
+  /// Direction is negative, the player moves to left.
+  ///
+  /// Direction is positive, the player moves to right.
+  void _updatePlayerMovement(double dt) {
+    double dirX = 0.0;
+
+    switch (playerDirection) {
+      case PlayerDirection.left:
+        if (facingRight) {
+          flipHorizontallyAroundCenter();
+          facingRight = false;
+        }
+
+        current = PlayerState.running;
+        dirX -= moveSpeed;
+        break;
+      case PlayerDirection.right:
+        if (!facingRight) {
+          flipHorizontallyAroundCenter();
+          facingRight = true;
+        }
+        current = PlayerState.running;
+        dirX += moveSpeed;
+        break;
+      case PlayerDirection.none:
+        current = PlayerState.idle;
+        dirX = 0.0;
+        break;
+      default:
+    }
+
+    velocity = Vector2(dirX, 0.0);
+    position += velocity * dt;
   }
 }
