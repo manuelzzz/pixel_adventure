30990fe611e3126f7cd194108a2fd5971b3cb325
Manel <manuelss.23.bil@gmail.com>
2023-10-28 19:19:07 -0300

    fix: Structure correction for components struct

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -3,8 +3,8 @@ import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
-import 'package:pixel_adventure/actors/player.dart';
-import 'package:pixel_adventure/levels/level.dart';
+import 'package:pixel_adventure/components/player.dart';
+import 'package:pixel_adventure/components/level.dart';
 
 class PixelAdventure extends FlameGame
     with HasKeyboardHandlerComponents, DragCallbacks {
