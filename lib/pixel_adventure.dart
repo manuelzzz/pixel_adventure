d5684b7823bf1a02115066a447052ed50f67f12e
Manel <manuelss.23.bil@gmail.com>
2023-10-27 23:09:16 -0300

    feat: Game class created

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -1,3 +1,26 @@
+import 'dart:async';
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:pixel_adventure/levels/level.dart';
 
-class PixelAdventure extends FlameGame {}
+class PixelAdventure extends FlameGame {
+  late final CameraComponent cam;
+
+  @override
+  final world = Level();
+
+  @override
+  FutureOr<void> onLoad() {
+    cam = CameraComponent.withFixedResolution(
+      world: world,
+      width: 640,
+      height: 360,
+    );
+
+    cam.viewfinder.anchor = Anchor.topLeft;
+
+    addAll([cam, world]);
+
+    return super.onLoad();
+  }
+}
