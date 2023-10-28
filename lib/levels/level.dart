00e06e53ef947c4cbb7a767f07472a4238901128
Manel <manuelss.23.bil@gmail.com>
2023-10-28 13:05:34 -0300

    feat: Player spawn in spawnpoint defined in tiled layer: Spawnpoints class: Player

--- lib/levels/level.dart
+++ lib/levels/level.dart
@@ -2,18 +2,37 @@ import 'dart:async';
 
 import 'package:flame/components.dart';
 import 'package:flame_tiled/flame_tiled.dart';
+import 'package:pixel_adventure/actors/player.dart';
 
 class Level extends World {
+  final String levelName;
   late TiledComponent level;
+
+  Level({required this.levelName});
 
   @override
   FutureOr<void> onLoad() async {
     level = await TiledComponent.load(
-      'Level-01.tmx',
+      '$levelName.tmx',
       Vector2.all(16),
     );
 
     add(level);
+
+    final spawnpointsLayer = level.tileMap.getLayer<ObjectGroup>('Spawnpoints');
+
+    for (final spawnPoint in spawnpointsLayer!.objects) {
+      switch (spawnPoint.class_) {
+        case 'Player':
+          final player = Player(
+            character: 'Mask Dude',
+            position: Vector2(spawnPoint.x, spawnPoint.y),
+          );
+          add(player);
+          break;
+        default:
+      }
+    }
 
     return super.onLoad();
   }
