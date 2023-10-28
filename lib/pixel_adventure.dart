2f355c1a408ee8454714d10de29ab5dc77885984
Manel <manuelss.23.bil@gmail.com>
2023-10-28 13:07:02 -0300

    style: Adding the background color to remove the black edge

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -1,16 +1,23 @@
 import 'dart:async';
+import 'dart:ui';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:pixel_adventure/levels/level.dart';
 
 class PixelAdventure extends FlameGame {
+  @override
+  Color backgroundColor() => const Color(0xFF211F30);
+
   late final CameraComponent cam;
 
   @override
-  final world = Level();
+  final world = Level(levelName: 'Level-01');
 
   @override
-  FutureOr<void> onLoad() {
+  Future<void> onLoad() async {
+    // Load all images into cache
+    await images.loadAllImages();
+
     cam = CameraComponent.withFixedResolution(
       world: world,
       width: 640,
