2fcff609d8f8e218a0b4e21fe8328a938817c4d5
Manel <manuelss.23.bil@gmail.com>
2023-10-27 23:08:55 -0300

    feat: adjusting the screen  with flame

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
-import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
 import 'package:pixel_adventure/pixel_adventure.dart';
 
 void main() {
-  runApp(GameWidget(
-    game: PixelAdventure(),
-  ));
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
+  Flame.device.setLandscape();
+
+  PixelAdventure game = PixelAdventure();
+  runApp(
+    GameWidget(game: kDebugMode ? PixelAdventure() : game),
+  );
 }
